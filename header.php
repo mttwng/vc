d65f26c550a75b6d5e1d715adeac42ce0f0abea1
Matthew Wong <mtthwwng@gmail.com>
2015-01-15 02:13:46 -0800

    Facebook Conversion Code for Apply

--- header.php
+++ header.php
@@ -65,6 +65,23 @@
 	</script>
 	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=727213910719134&amp;ev=PixelInitialized" /></noscript>
 
+	<!-- Facebook Conversion Code for Apply -->
+	<script>(function() {
+	var _fbq = window._fbq || (window._fbq = []);
+	if (!_fbq.loaded) {
+	var fbds = document.createElement('script');
+	fbds.async = true;
+	fbds.src = '//connect.facebook.net/en_US/fbds.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(fbds, s);
+	_fbq.loaded = true;
+	}
+	})();
+	window._fbq = window._fbq || [];
+	window._fbq.push(['track', '6021097818450', {'value':'0.01','currency':'USD'}]);
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6021097818450&amp;cd[value]=0.01&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
+
 	<?php if ($page == 'apply.php') { ?>
 			<meta http-equiv="refresh" content="0; url=https://voyager-consulting.workable.com/jobs/31573" />
 		<?php } 
