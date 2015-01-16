273e6e9debc15d65bc5c0e20b72a6400b0e87641
Matthew Wong <mtthwwng@gmail.com>
2015-01-16 14:51:26 -0800

    minor button changes

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 			<p>At the heart of Voyager, there is more than a consulting club;
 			there is a family who strives to create an enriching personal and
 			professional experience for every member.</p>
-			<a id="end-transparent" type="button" href="careers.php" class="btn btn-default whitebutton">Careers</a>
+			<a id="end-transparent" type="button" href="about.php" class="btn btn-default whitebutton">About</a>
 		</div>
 	</div>
 	<div class="down-arrow">
@@ -42,7 +42,7 @@
 			<p>Consultants are selected from a diverse range of backgrounds and rank
 			 at the top of their classes. Alumni have gone to prestigious institutions
 			  such as Goldman Sachs, Bain &amp; Company, and Apple.</p>
-			<a type="button" href="about.php" class="btn btn-default whitebutton">About</a>
+			<a type="button" href="careers.php" class="btn btn-default whitebutton">Careers</a>
 		</div>
 	</div>
 	<div class="down-arrow">
